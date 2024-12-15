eba0a07d9a0320dd65bdb7c912a60c99e2dd709d
Naily <zero@naily.cc>
2024-12-15 15:16:11 +0800

    feat: remove typeorm

--- naily.config.ts
+++ naily.config.ts
@@ -1,15 +1,9 @@
-import path from 'node:path'
 import { defineConfig } from '@nailyjs/config'
 
 export default defineConfig({
   naily: {
     app: {
       internalCookie: 'MUSIC_U',
-    },
-
-    typeorm: {
-      type: 'sqlite',
-      database: path.resolve('./data/naily.db'),
     },
   },
 })
