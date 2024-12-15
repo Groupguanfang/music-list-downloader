a2d458d7bb3692ad98d14fd4380d182b9aaaab0c
Naily <zero@naily.cc>
2024-12-15 12:56:08 +0800

    feat: update main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,9 @@
 import path from 'node:path'
 import process from 'node:process'
 import { ElectronAdapter } from '@nailyjs/electron'
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
 import { app as rpc } from '../backend/main'
+import { version } from '../package.json'
 
 let __dirname: string = globalThis.__dirname
 if (!__dirname)
@@ -12,10 +13,53 @@ app.whenReady().then(async () => {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    minWidth: 400,
     webPreferences: {
       preload: path.resolve(decodeURIComponent(__dirname), './preload.cjs'),
     },
   })
+
+  app.setAboutPanelOptions({
+    applicationName: '网易云音乐下崽器',
+    applicationVersion: version,
+    authors: ['Naily Zero <zero@naily.cc> (https://naily.cc)'],
+    version,
+  })
+
+  Menu.setApplicationMenu(
+    Menu.buildFromTemplate([
+      {
+        label: '网易云音乐下崽器',
+        submenu: [
+          {
+            label: '关于',
+            role: 'about',
+          },
+          {
+            label: '开发者选项',
+            submenu: [
+              {
+                label: '开发者工具',
+                role: 'toggleDevTools',
+              },
+              {
+                label: '重新加载',
+                role: 'reload',
+              },
+              {
+                label: '强制重新加载',
+                role: 'forceReload',
+              },
+            ],
+          },
+          {
+            label: '退出',
+            role: 'quit',
+          },
+        ],
+      },
+    ]),
+  )
 
   await rpc.setBackendAdapter(ElectronAdapter)
     .getRpcMethodExecutor()
